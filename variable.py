163a9e9ee1d15f4423ea126180621b0483f4d0bb
SoshiantTeam <soshiant.ithub@gmail.com>
2022-04-21 15:27:46 +0430

    add new files

---
+++ variable.py
@@ -0,0 +1,34 @@
+
+# variable
+voorodi=12.5
+print("--"*20)
+if type(voorodi)==float:
+  print('voorodi is float')
+else:
+   print("Error")
+    
+    
+name_str="salam"
+#print(int(name_str))
+
+name_str2="259945"
+n2=int(name_str2)
+#print(n2)
+#print(type(n2))
+
+name_str3=259945
+n3=str(name_str2)
+#print(n3)
+#print(type(n3))
+print("--"*20)
+x=4
+y=8
+b=x
+x=y
+y=b
+#print(x)
+#print(y)
+a,b= 5,15
+a,b=a*2,a+b
+#print(a)
+#print(b)
